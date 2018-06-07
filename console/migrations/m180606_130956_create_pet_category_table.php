c34418202db7bda1fd7a4b46d6b7900d85c755ff
ARKKupendran <35802459+ARKKupendran@users.noreply.github.com>
2018-06-07 11:34:11 +0530

    Files Uploaded

---
+++ console/migrations/m180606_130956_create_pet_category_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `pet_category`.
+ */
+class m180606_130956_create_pet_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('pet_category', [
+            'pet_cat_id' => $this->primaryKey(),
+            'category_name' => $this->string()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->Null(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('pet_category');
+    }
+}
